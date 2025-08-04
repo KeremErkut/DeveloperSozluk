f98efccd4baea641c79663ec6d0374deb97836d0
erkut <kciftlikci@gmail.com>
2025-08-04 05:30:32 +0300

    Create basic views: Added home and topic detail view with simple HTTP responses.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    # View function for homepage.
+    # It will diplay a list of all topics
+    return HttpResponse("This will be the home page, displaying all topics.")
+
+def topic_detail(request, topic_id):
+    # View function for a specific topic's detail page.
+    # It will display all entries under that topic
+    return HttpResponse(f"This will be the detail page for topic with ID: {topic_id}")
+
